f234593292f0703080ad735c32744dc55abdad7a
Daniele Santini <danysan95@gmail.com>
2016-11-01 10:35:14 +0100

    Completate funzioni di rete

--- rete.c
+++ rete.c
@@ -12,112 +12,141 @@
 #define PORTA_SERVER 54321
 
 int creaServer(){
-    const int sockFD = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    int out = sockFD;
-    
-    if(sockFD < 0)
-        perror("Errore nella creazione della socket di ascolto");
-    else {
-        struct sockaddr_in local;
-        local.sin_family = AF_INET;
-        local.sin_port = htons(PORTA_SERVER);
-        local.sin_addr.s_addr = INADDR_ANY;
-        
-        const int risBind = bind(sockFD, (struct sockaddr*)&local, sizeof(struct sockaddr_in));
-        
-        if(risBind < 0){
-            perror("Errore nella bind()");
-            out = risBind;
-        } else {
-            const int risListen = listen(sockFD, 2);
-            
-            if(risListen < 0){
-                perror("Errore nella listen()");
-                out = risListen;
-            }
-        }
-        
-        if(out < 0)
-            close(sockFD);
-    }        
-    
-    return out;
+	const int sockFD = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	int out = sockFD;
+	
+	if(sockFD < 0)
+		perror("Errore nella creazione della socket di ascolto");
+	else {
+		struct sockaddr_in local;
+		local.sin_family = AF_INET;
+		local.sin_port = htons(PORTA_SERVER);
+		local.sin_addr.s_addr = INADDR_ANY;
+		
+		const int risBind = bind(sockFD, (struct sockaddr*)&local, sizeof(struct sockaddr_in));
+		
+		if(risBind < 0){
+			perror("Errore nella bind()");
+			out = risBind;
+		} else {
+			const int risListen = listen(sockFD, 2);
+			
+			if(risListen < 0){
+				perror("Errore nella listen()");
+				out = risListen;
+			}
+		}
+		
+		if(out < 0)
+			close(sockFD);
+	}        
+	
+	return out;
 }
 
 
 
 int attendiConnessione(const int socketAscolto){
-    int out;
-    
-    do {
-        out = accept(socketAscolto, 0, 0);
-    } while (errno == EINTR);
-    
-    return out;
-}
-
-
-
-int inviaDimensione(const int socket, const int dim){
-    const int tradotto = htons(dim);
-    return (sizeof(tradotto) == write(socket, &tradotto, sizeof(tradotto)));
+	int out;
+	
+	do {
+		out = accept(socketAscolto, 0, 0);
+	} while (errno == EINTR);
+	
+	return out;
 }
 
 
 
 int creaClient(char *indirizzo){
-    int sockFD, risConnect;
-    struct sockaddr_in server;
-    struct hostent *serverInfo;
-    
-    if(indirizzo == 0)
-        return -1;
-    
-    serverInfo = gethostbyname(indirizzo);
-    
-    if(serverInfo == 0)
-        return -1;
-
-    server.sin_family = AF_INET;
-    server.sin_port = htons(PORTA_SERVER);
-    server.sin_addr.s_addr = ((struct in_addr*) (serverInfo->h_addr))->s_addr;
-    
-    sockFD = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    
-    if(sockFD < 0){
-        perror("Errore nella creazione della socket");
-        return -1;
-    }
-    
-    do {
-        risConnect = connect(sockFD, (struct sockaddr*)&server, sizeof(server));
-    } while (errno == EINTR);
-        
-    if(risConnect < 0){
-        perror("Errore nella connect()");
-        return risConnect;
-    }
-    
-    
-    return sockFD;
+	int sockFD, risConnect;
+	struct sockaddr_in server;
+	struct hostent *serverInfo;
+	
+	if(indirizzo == 0)
+		return -1;
+	
+	serverInfo = gethostbyname(indirizzo);
+	
+	if(serverInfo == 0)
+		return -1;
+	
+	server.sin_family = AF_INET;
+	server.sin_port = htons(PORTA_SERVER);
+	server.sin_addr.s_addr = ((struct in_addr*) (serverInfo->h_addr))->s_addr;
+	
+	sockFD = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	
+	if(sockFD < 0){
+		perror("Errore nella creazione della socket");
+		return -1;
+	}
+	
+	do {
+		risConnect = connect(sockFD, (struct sockaddr*)&server, sizeof(server));
+	} while (errno == EINTR);
+	
+	if(risConnect < 0){
+		perror("Errore nella connect()");
+		return risConnect;
+	}
+	
+	
+	return sockFD;
 }
 
 
 
-int riceviDimensione(const int socket){
-    int dim, ricevuti = read(socket, &dim, sizeof(dim));
-    
-    if(ricevuti != sizeof(dim))
-        dim = -1;
-    else
-        dim = ntohs(dim);
-        
-    return dim;
+int inviaInt(const int socket, int val){
+	val = htons(val);
+	return (sizeof(val) == write(socket, &val, sizeof(val)));
+}
+
+
+
+int riceviInt(const int socket){
+	int val, ricevuti = read(socket, &val, sizeof(val));
+	
+	if(ricevuti == sizeof(val))
+		return ntohs(val);
+	else
+		return -1;
+}
+
+
+
+int inviaMappa(const int socket, const int dimensione, int **mappa){
+	int count=0;
+	
+	for(int i=0; i<dimensione; i++){
+		for(int k=0; k<dimensione; k++){
+			int val = htons(mappa[i][k]);
+			count += write(socket, &val, sizeof(val));
+		}
+	}
+	
+	return count == ((int)sizeof(int)*dimensione*dimensione);
+}
+
+
+
+int riceviMappa(const int socket, const int dimensione, int **buffer){
+	int count=0;
+	
+	for(int i=0; i<dimensione; i++){
+		for(int k=0; k<dimensione; k++){
+			int val;
+			count += read(socket, &val, sizeof(val));
+			buffer[i][k] = ntohs(val);
+		}
+	}
+	
+	return count == ((int)sizeof(int)*dimensione*dimensione);
 }
 
 
 
 int chiudi(const int socket){
-    shutdown(socket, 2);
-    return close(socket);
+	shutdown(socket, 2);
+	return close(socket);
 }
--- rete.h
+++ rete.h
@@ -4,17 +4,15 @@
 // Funzioni Server
 int creaServer(); // Prepara una socket di ascolto e restituisce il file descriptor (o <0 in caso di errore)
 int attendiConnessione(const int socketAscolto); // Attende che un client si connetta e restituisce la nuova socket (o <0 in caso di errore)
-int inviaDimensione(const int socket, const int dim); // Invia la dimensione del campo di gioco (dim==-1 -> Partita rifiutata)
 
 // Funzioni Client
 int creaClient(char *indirizzo); // Si connette ad un server e restituisce la socket (o <0 in caso di errore)
-int riceviDimensione(const int socket); // Riceve la dimensione del campo di gioco (dim==-1 -> errore di comunicazione o partita rifiutata)
 
 // Funzioni comuni
-int inviaMossa(const int socket, const int riga, const int colonna); // 
-int inviaMappa(const int socket, int **mappa);
-int riceviMossa(const int socket, int *riga, int *colonna); // Inserisce la mossa ricevuta nelle variabili puntate
-int ** riceviMappa(const int socket);
+int inviaInt(const int socket, int val);
+int riceviInt(const int socket);
+int inviaMappa(const int socket, const int dimensione, int **mappa);
+int riceviMappa(const int socket, const int dimensione, int **buffer);
 int chiudi(const int socket); // Termina la comunicazione e chiude la socket
 
 #endif
